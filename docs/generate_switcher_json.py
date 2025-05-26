fb1184f16c7484a2b475738c0ae11104a1217c09
fzahle <frza@dtu.dk>
2025-05-26 22:11:54 +0200

    also for switcher file

--- docs/generate_switcher_json.py
+++ docs/generate_switcher_json.py
@@ -10,7 +10,7 @@ STATIC_DIR = os.path.join(REPO_DIR, 'docs', '_static')
 # relies on soft link of latest tag to "latest"    
 latest_branch = "latest"
 
-DEPLOY_URL = os.environ.get("DEPLOY_URL", "https://ieawindsystems.github.io")
+DEPLOY_URL = os.environ.get("DEPLOY_URL", "https://ieawindsystems.github.io/windIO")
 
 SWITCHER_JSON_PATH = os.path.join(STATIC_DIR, 'switcher.json')
 
